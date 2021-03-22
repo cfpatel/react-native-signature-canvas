20149a5a13e5484bdbe913aade29816ed995aa48
Chetan Patel <38491657+cfpatel@users.noreply.github.com>
2021-03-22 16:12:27 +0400

    Update index.js
    Updated border radius top and bottom right for webView component

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ const SignatureView = forwardRef(({
         javaScriptEnabled={true}
         onError={renderError}
         onLoadEnd={() => setLoading(false)}
-        style={{borderRadius: 10}} // Chetan - Added 
+        style={{borderTopRightRadius: 10, borderBottomRightRadius: 10}} // Chetan - Added 
       />
       {loading && <View style={styles.loadingOverlayContainer}>
         <ActivityIndicator />
