85b31e6ad2d39bdd12b376565f4987ed7a161244
Chetan Patel <38491657+cfpatel@users.noreply.github.com>
2021-03-22 15:58:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { WebView } from "react-native-webview";
 const styles = StyleSheet.create({
   webBg: {
     width: "100%",
-    backgroundColor: "#FFF",
+    backgroundColor: "transparent",
     flex: 1
   },
   loadingOverlayContainer: { position: "absolute", top: 0, bottom: 0, left: 0, right: 0, alignItems: "center", justifyContent: "center" },
